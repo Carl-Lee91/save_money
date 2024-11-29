020bf3f054177ed140c03e2d484df1da18c4391c
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2024-11-29 22:23:18 +0900

    ✨ feat: add view at record view

--- lib/feat/dashboard/record/record_view.dart
+++ lib/feat/dashboard/record/record_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
+import 'package:save_money/feat/dashboard/record/widget/output_history_widget.dart';
+import 'package:save_money/feat/dashboard/widget/arrow_row_widget.dart';
 import 'package:save_money/feat/dashboard/widget/rec_btn.dart';
 import 'package:save_money/theme/app_colors.dart';
 
@@ -17,8 +20,66 @@ class RecordView extends ConsumerWidget {
         child: Column(
           children: [
             _homeTop(context),
+            _outputDetail(context),
+            const OutputHistoryWidget(
+              budgetCategory: '지출',
+              time: '오전 11시 40분',
+              category: '배달',
+              output: '-30,000원',
+            ),
+            const OutputHistoryWidget(
+              budgetCategory: '지출',
+              time: '오전 11시 40분',
+              category: '배달',
+              output: '-30,000원',
+              memo: '관리비',
+            ),
+            const OutputHistoryWidget(
+              budgetCategory: '지출',
+              time: '오전 11시 40분',
+              category: '배달',
+              output: '-30,000원',
+            ),
           ],
         ),
+      ),
+      floatingActionButton: SvgPicture.asset(
+        ThemeMode.system == ThemeMode.dark
+            ? 'assets/svg/dashboard/add_dark.svg'
+            : 'assets/svg/dashboard/add_light.svg',
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+
+  Widget _outputDetail(context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 20, 16, 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            '소비기록 (3건)',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                  color: AppColors.mainTextColor,
+                ),
+          ),
+          ArrowRowWidget(
+            leftGap: 8,
+            rightGap: 8,
+            leftOnTap: () {},
+            rightOnTap: () {},
+            contextWidget: Text(
+              '24.11.10',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    fontSize: 16,
+                    color: AppColors.mainTextColor,
+                  ),
+            ),
+          ),
+        ],
       ),
     );
   }
