6263dbb0f49aa605eff948665f44a63d8800f300
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2024-11-29 22:23:38 +0900

    ✨ feat: complete widget

--- lib/feat/dashboard/record/widget/output_history_widget.dart
+++ lib/feat/dashboard/record/widget/output_history_widget.dart
@@ -8,10 +8,16 @@ class OutputHistoryWidget extends ConsumerWidget {
     super.key,
     required this.budgetCategory,
     required this.time,
+    required this.category,
+    required this.output,
+    this.memo,
   });
 
   final String budgetCategory;
   final String time;
+  final String category;
+  final String output;
+  final String? memo;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -28,33 +34,117 @@ class OutputHistoryWidget extends ConsumerWidget {
             ? AppColors.mainTextColor
             : AppColors.whiteColor,
       ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        child: Column(
-          children: [
-            Row(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Column(
               children: [
-                Text(budgetCategory),
-                const Gap(8),
-                const Divider(
-                  thickness: 1,
-                  height: 6,
-                  color: ThemeMode.system == ThemeMode.dark
-                      ? AppColors.subText2Color
-                      : AppColors.lineSubTextColor,
+                Row(
+                  children: [
+                    Text(
+                      budgetCategory,
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            fontSize: 14,
+                            color: AppColors.mainTextColor,
+                          ),
+                    ),
+                    const Gap(8),
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: ThemeMode.system == ThemeMode.dark
+                              ? AppColors.subText2Color
+                              : AppColors.lineSubTextColor,
+                        ),
+                      ),
+                      child: const SizedBox(
+                        height: 6,
+                        width: 1,
+                      ),
+                    ),
+                    const Gap(8),
+                    Text(
+                      time,
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.lightSubTextColor,
+                          ),
+                    ),
+                  ],
                 ),
-                const Gap(8),
-                Text(time),
+                const Gap(12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    DecoratedBox(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(6)),
+                        color: AppColors.kakaoLoginColor,
+                      ),
+                      child: SizedBox(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 4,
+                          ),
+                          child: Text(
+                            category,
+                            style:
+                                Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                      fontSize: 14,
+                                      color: AppColors.whiteColor,
+                                    ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      output,
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                            color: AppColors.mainTextColor,
+                          ),
+                    ),
+                  ],
+                ),
               ],
             ),
-            const Gap(12),
-            const Row(
-              children: [
-                //TODO: 여기에 카테고리 및 금액 넣기
-              ],
+          ),
+          if (memo != null)
+            Container(
+              decoration: const BoxDecoration(
+                color: AppColors.bgColor200,
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                child: Row(
+                  children: [
+                    Text(
+                      '지출메모: ',
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.lightSubTextColor,
+                          ),
+                    ),
+                    const Gap(4),
+                    Text(
+                      memo!,
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.lightSubTextColor,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ],
-        ),
+        ],
       ),
     );
   }
