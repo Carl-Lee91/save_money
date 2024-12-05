4ecc11914f95f5275b6a684280c8a405fd6f6856
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2024-12-05 21:27:57 +0900

    ✨ fix: fix app bar

--- lib/feat/global_widget/app_bar/app_bar.dart
+++ lib/feat/global_widget/app_bar/app_bar.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+import 'package:save_money/feat/setting/setting_view.dart';
 
 class MainAppBar extends ConsumerWidget implements PreferredSizeWidget {
   const MainAppBar({
@@ -10,6 +12,10 @@ class MainAppBar extends ConsumerWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    void navigateSettingView() {
+      context.pushNamed(SettingView.routeName);
+    }
+
     return AppBar(
       actions: [
         SvgPicture.asset(
@@ -18,10 +24,13 @@ class MainAppBar extends ConsumerWidget implements PreferredSizeWidget {
           height: 24,
         ),
         const Gap(12),
-        SvgPicture.asset(
-          'assets/svg/app_bar/setting.svg',
-          width: 24,
-          height: 24,
+        GestureDetector(
+          onTap: navigateSettingView,
+          child: SvgPicture.asset(
+            'assets/svg/app_bar/setting.svg',
+            width: 24,
+            height: 24,
+          ),
         ),
         const Gap(12),
       ],
